efd05ff78ae89b3e59c03110780afa58e90a076e
piotrbojda1 <kontakt@piotrbojda.pl>
2023-05-16 21:09:40 +0200

    Add Route class to database

--- website/models.py
+++ website/models.py
@@ -7,3 +7,12 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+
+class Route(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String(150))
+    destination = db.Column(db.String(150))
+    length_route = db.Column(db.String(150))
+    travel_time = db.Column(db.String(150))
+    avoid = db.Column(db.String(150))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
